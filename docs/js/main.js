8585831a764bdd6b9b29a18b372c8e09341beefa
Artem <xthemey@gmail.com>
2020-11-02 00:42:18 +0300

    Head

---
+++ docs/js/main.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+	var formItem = document.querySelectorAll('.form-item input');
+
+
+
+
+	formItem.forEach(function (item) {
+		const placeHolderItem = item.nextElementSibling;
+
+		item.addEventListener('focus', function () {
+
+			placeHolderItem.classList.add("form__item-active")
+		})
+
+		item.addEventListener('blur', function () {
+			if (item.value === "") {
+				placeHolderItem.classList.remove("form__item-active")
+			}
+		})
+	})
+
+})
